44f41e9558f0a318323a125f7e5c2259f97ed91b
elinejasund <elinejasund@gmail.com>
2024-11-06 15:40:26 +0100

    begynt på del 2

--- todo.js
+++ todo.js
@@ -1,1 +1,13 @@
+let addbar = document.getElementById("taskadd")
+let list = document.getElementById("tasks")
+let button = document.getElementById("button")
 
+window.addEventListener("load", function(){
+    addbar.focus()
+})
+
+button.addEventListener("click", addTask)
+
+function addTask(){
+    list.innerHTML += '<li>' + '<input type="checkbox" id ="todo">'+ addbar.value + '</inpput></li>'
+}
