b76a9901eff19957b07ea0c92eede3038880fc40
elinejasund <elinejasund@gmail.com>
2024-11-06 09:40:52 +0100

    begynte på oppgave, men ikke sikker på om de er riktige

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 /* Task  1.2 */
-console.log('Task  1.2')
+for (let i = 1; i < 21; i++){
+    console.log(i)
+}
 
 /* Task  1.3 */
-console.log('Task  1.3')
-
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 3 === 0){
+      console.log("eple")
+    }
+    else if (numbers[i] % 5 === 0){
+      console.log("kake")
+    }
+     
+    else{
+      console.log(numbers[i])
+    }
+  }
+
 /* Task  1.4 */
+let headline = document.querySelector("h1")
+headline.innerText = "Hello, JavaScript"
 
 /* Task  1.5 */
 function changeDisplay () {
