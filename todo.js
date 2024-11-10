022d84137050695dd1a4dfdfd6017502b6af56e7
elinejasund <elinejasund@gmail.com>
2024-11-10 19:04:54 +0100

    gjort mye ferdig

--- todo.js
+++ todo.js
@@ -1,22 +1,58 @@
 let addbar = document.getElementById("taskadd")
-let taskList = document.getElementById("tasks")
+let taskList = document.getElementById("taskslist")
 let button = document.getElementById("button")
+let tasks = []
+let i = 0;
+let count = document.getElementById("count")
+let tasksDone = 0
+
+
+
+button.addEventListener("click", addTask)
+
+taskList.addEventListener("change", checked)
 
 window.addEventListener("load", function(){
     addbar.focus()
 })
 
-button.addEventListener("click", addTask)
+function checked(ev){
+    let endret = ev.target
+    li = endret.parentElement
+    li.classList.toggle("checked")
+    if (li.classList.contains("checked")) {
+        li.setAttribute('style', 'text-decoration: line-through');
+        tasksDone += 1
+        count.innerText = (tasksDone + "/" + (i + 1))
 
-let list = []
-function addTask(){
-    list.unshift('<li><input type="checkbox" id ="todo">'+ addbar.value + '</inpput></li>')
-    console.log(list)
-
-    taskList.innerHTML = null
-    for (let i = 0; i < list.length; i++) {
-        taskList.innerHTML += list[i]
+    } else{
+        li.setAttribute('style', 'text-decoration: none');
+        tasksDone -= 1
+        count.innerText = (tasksDone + "/" + (i + 1))
     }
 }
+
+
+function addTask(){
+    
+    if (tasks.length == 0) {
+        tasks.unshift('<li><input type="checkbox" class ="todo" id ="todo' + 0 + '">' + addbar.value + '</input></li>')
+    }
+
+    else{
+        i += 1
+        tasks.unshift('<li><input type="checkbox" class ="todo" id ="todo' + i + '">' + addbar.value + '</input></li>')
+    }
+
+    taskList.innerHTML = null
+    for (let i = 0; i < tasks.length; i++) {
+        taskList.innerHTML += tasks[i]
+    }
+    count.innerText = (tasksDone + "/" + (i + 1))
+
+}
+
+
+
 
 
