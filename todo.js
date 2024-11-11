48f608fd02377b52fc9cde3e00a50bf2f92bcc51
emilfs <emilfsalomonsen@gmail.com>
2024-11-11 15:03:32 +0100

    validert js

--- todo.js
+++ todo.js
@@ -1,34 +1,33 @@
-let addbar = document.getElementById("taskadd")
-let taskList = document.getElementById("taskslist")
-let button = document.getElementById("button")
-let tasks = []
+/*jshint esversion: 6 */
+let addbar = document.getElementById("taskadd");
+let taskList = document.getElementById("taskslist");
+let button = document.getElementById("button");
+let tasks = [];
 let i = 0;
-let count = document.getElementById("count")
-let tasksDone = 0
+let count = document.getElementById("count");
+let tasksDone = 0;
 
+button.addEventListener("click", addTask);
 
-
-button.addEventListener("click", addTask)
-
-taskList.addEventListener("change", checked)
+taskList.addEventListener("change", checked);
 
 window.addEventListener("load", function(){
-    addbar.focus()
-})
+    addbar.focus();
+});
 
 function checked(ev){
-    let endret = ev.target
-    li = endret.parentElement
-    li.classList.toggle("checked")
+    let endret = ev.target;
+    li = endret.parentElement;
+    li.classList.toggle("checked");
     if (li.classList.contains("checked")) {
         li.setAttribute('style', 'text-decoration: line-through');
-        tasksDone += 1
-        count.innerText = (tasksDone + "/" + (i + 1))
+        tasksDone += 1;
+        count.innerText = (tasksDone + "/" + (i + 1));
 
     } else{
         li.setAttribute('style', 'text-decoration: none');
-        tasksDone -= 1
-        count.innerText = (tasksDone + "/" + (i + 1))
+        tasksDone -= 1;
+        count.innerText = (tasksDone + "/" + (i + 1));
     }
 }
 
@@ -36,23 +35,23 @@ function checked(ev){
 function addTask(){
     
     if (tasks.length == 0) {
-        tasks.unshift('<li><input type="checkbox" class ="todo">' + addbar.value + '</input></li>')
+        tasks.unshift('<li><input type="checkbox" class ="todo">' + addbar.value + '</input></li>');
     }
 
     else{
-        i += 1
-        tasks.unshift('<li><input type="checkbox" class ="todo">' + addbar.value + '</input></li>')
+        i += 1;
+        tasks.unshift('<li><input type="checkbox" class ="todo">' + addbar.value + '</input></li>');
     }
 
-    taskList.innerHTML = null
-    tasksDone = 0
-    addbar.value = ""
+    taskList.innerHTML = null;
+    tasksDone = 0;
+    addbar.value = "";
 
     for (let i = 0; i < tasks.length; i++) {
-        taskList.innerHTML += tasks[i]
+        taskList.innerHTML += tasks[i];
     }
-    count.innerText = (tasksDone + "/" + (i + 1))
-    console.log(tasks)
+    count.innerText = (tasksDone + "/" + (i + 1));
+    console.log(tasks);
 }
 
 
